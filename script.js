9acafac65281d462ea94d69a659c79091d232a10
dsingh44 <80799486+dsingh44@users.noreply.github.com>
2023-06-04 05:15:21 +0000

    Implemented navigation bar

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ let bg = document.getElementById("bg");
 let bird =document.getElementById("bird");
 let mountains =document.getElementById("mountains");
 let nametext = document.getElementById("nametext");
+let darkmode = document.getElementById("darkmode-button");
 
 let rates={
   bg: 0.05,
   bird: 2.5,
   mountains: 0.15,
-  text:16
+  text:14
 };
+
+const DarkMode = ()=>{
+  document.body.classList.toggle("dark-mode");
+}
+//event listener
+darkmode.addEventListener("click",DarkMode);
+
+
 // This function handles scroll event
 const handlescroll = ()=> {
   let scrollDistance = window.scrollY;
